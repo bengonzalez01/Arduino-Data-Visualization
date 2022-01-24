1f6d368a409d191064c22757d2fb34cb3c587879
Ben Gonzalez <gonzalezbj@g.cofc.edu>
2022-01-24 18:33:01 -0500

    Start Dash server

--- Dash_Plotly_Viz.py
+++ Dash_Plotly_Viz.py
@@ -9,7 +9,37 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+# starting app
 app = dash.Dash(__name__)
+
 # reading data into csv
 df = pd.read_csv('arduino_test_data.csv')
 
+# creating the app layout
+app.layout = html.Div([
+
+    html.H1("Arduino Data Visualization", style= {'text-align': 'center'}),
+
+    dcc.Dropdown(id = 'Select Data Type',
+                options= [
+                    {"label" : "Pressure", "value" : "Pressure"},
+                    {"label" : "Altitude", "value" : "Altitude"},
+                    {"label" : "Light", "value" : "Light"},
+                    {"label" : "Sound", "value" : "Sound"},
+                    {"label" : "Temperature", "value" : "Temperature"},
+                    {"label" : "Humidity", "value" : "Humidity"}],
+                    multi= False,
+                    value= "Pressure",
+                    style={'width' : "40%"}
+                    ),
+
+    
+    html.Div(id= 'output_container', children=[]),
+    html.Br(),
+
+    dcc.Graph(id = "environmental_values", figure = {})
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
